7d34a756ded1d13133f2b141830e7ec235b802fd
Ren-yuhao <1178082508l@qq.com>
2021-10-04 17:50:28 +0800

    Site updated: 2021-10-04 17:50:28

--- js/wrapImage.js
+++ js/wrapImage.js
@@ -1,17 +1,19 @@
 $(document).ready(function () {
   wrapImageWithFancyBox();
 });
+
 /**
 * Wrap images with fancybox support.
 */
 function wrapImageWithFancyBox() {
+  $.fancybox.defaults.hash = false;
   $(".post-detail").find("img").not('.swiper-slide img').not('.sidebar-image img').not('#author-avatar img').not(".post-donate img").not(".post-donate img").not(".friend-avatar img").not('[title=notice]').not("#myComment img").not(".social-share .qrcode img").not(".ghcard img").not('img.inline').not(".site-card img").not(".link-card img").not('.btns img').each(function () {
     var $image = $(this);
     var imageCaption = $image.attr('alt');
     var $imageWrapLink = $image.parent('a');
     var $linkWrapDiv = $imageWrapLink.parent('div');
     if ($imageWrapLink.length < 1) {
-      var src = this.getAttribute('data-original') || this.getAttribute('src');
+      var src = this.getAttribute('data-src') || this.getAttribute('src');
       var idx = src.lastIndexOf('?');
       if (idx != -1) {
         src = src.substring(0, idx);
